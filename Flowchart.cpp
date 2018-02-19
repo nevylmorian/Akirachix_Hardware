ef2b89f307c14b5b34a6143114fe87c9a6d5968b
Maurine Onduko <maurineonduko96@gmail.com>
2018-02-19 16:14:38 +0300

    added a file

---
+++ Flowchart.cpp
@@ -0,0 +1,38 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a;
+    int b;
+
+ cout<<"ask for the first number";
+cin>>a;
+cout<<"ask for the second number";
+cin>>b;
+if(a>b){
+    cout<<"first number is greater ";
+    
+}
+else if(a==b){
+    cout<<"first number is equal to second number";
+    
+}
+
+else{cout<<"second number is greater ";}
+
+    
+
+    return 0;
+}
+
+
+
